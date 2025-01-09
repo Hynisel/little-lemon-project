f0d1adff50d1625209690494199b3a5591b3a384
Hynisel <53999494+Hynisel@users.noreply.github.com>
2025-01-09 11:32:02 +0100

    Fix minor rendering problems in the about page

--- src/About.js
+++ src/About.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import './About.css';
 import ownerImage1 from './Assets/owner1.jpg';
@@ -8,8 +7,8 @@ const About = () => {
   return (
     <div className="about-container">
       <div className="text-section">
-      <p id="tittle_t">Little Lemon</p>
-      <p id="tittle_l">Chicago</p>
+        <p id="tittle_t">Little Lemon</p>
+        <p id="tittle_l">Chicago</p>
         <p>Welcome to our restaurant! We pride ourselves on delivering the finest Mediterranean cuisine in a welcoming atmosphere.</p>
         <p>Our team is dedicated to providing excellent service and ensuring every meal is a memorable experience.</p>
         <p>Meet the owners, who are passionate about food and hospitality, committed to making your dining experience exceptional.</p>
@@ -17,8 +16,8 @@ const About = () => {
       </div>
       <div className="image-section">
         <div className="image-container">
-          <img src={ownerImage1} alt="Owner_1" className="owner-image image-1" />
-          <img src={ownerImage2} alt="Owner_2" className="owner-image image-2" />
+          <img src={ownerImage1} alt="Owner_1" className="owner-image" />
+          <img src={ownerImage2} alt="Owner_2" className="owner-image" />
         </div>
       </div>
     </div>
